51ac7ce0042b9a6b8c9853c3c74d6bbac41c66db
Subhan383 <abdulsubhan0383@gmail.com>
2020-04-19 15:39:54 +0530

    Removed comments

--- HibernateCurdOperations5.2.8/src/org/hibernate/AddingData.java
+++ HibernateCurdOperations5.2.8/src/org/hibernate/AddingData.java
@@ -21,23 +21,10 @@ public class AddingData {
 		try {
 			
 			User user=new User("ssubhan", "sailajasubhan", "kanna", "Subhan");
-//			List<User> list=new LinkedList<>();
-//			User user1=new User("ssubhan", "sailajasubhan", "kanna", "Subhan");
-//			User user2=new User("sailu", "sailajasubhan", "kanna", "Azma");
-//			User user3=new User("arhaa", "arhaajasubhan", "arhaashaik", "Arhaa");
-//			User user4=new User("arhaan", "arhaanjasubhan", "arhaanshaik", "Arhaan");
-//			User user5=new User("aarohi", "aarohisubhan", "aarohishaik", "Aarohi");
-//			list.add(user1);
-//			list.add(user2);
-//			list.add(user3);
-//			list.add(user4);
-//			list.add(user5);
+
 			Transaction trans=session.beginTransaction();
 			session.save(user);
-//			session.save(user2);
-//			session.save(user3);
-//			session.save(user4);
-//			session.save(user5);
+
 			session.getTransaction().commit();
 			System.out.println("Rows added!");
 			
